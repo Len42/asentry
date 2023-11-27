23b675435795cb1e67669f0942b35dbddff132a8
Len Popp len.popp@gmail.com <len.popp@gmail.com>
2023-11-26 19:03:20 -0500

    add a note about the alert sound file

--- asentry.py
+++ asentry.py
@@ -4,6 +4,9 @@
     and displays an alert message if there are any new or increased threats
     since the last time it ran.
 
+    If there is a sound file named "alert.mp3" in this directory, it will be
+    played when a warning message is displayed.
+
     Copyright 2023 Len Popp - see LICENSE
 """
 
