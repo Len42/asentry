2e20d733c4e2278e9cde60cc66c71908260accdf
Len Popp len.popp@gmail.com <len.popp@gmail.com>
2023-11-27 13:13:52 -0500

    add a link to the JPL Sentry site in the documentation

--- asentry.py
+++ asentry.py
@@ -1,6 +1,7 @@
 """ asentry - Monitor the JPL Sentry database for asteroid impact threats.
 
     This program downloads a list of potential Earth-threatening asteroids
+    from NASA JPL's Sentry (https://cneos.jpl.nasa.gov/sentry/) service
     and displays an alert message if there are any new or increased threats
     since the last time it ran.
 
@@ -9,7 +10,7 @@
 
     If any warning messages are displayed, the program waits for the user to
     press Enter before exiting; otherwise it exits immediately. This is because
-    I have the program run at system startup and I need its window to stay open
+    I have the program run at system startup and I want its window to stay open
     so I can read the messages.
 
     Copyright 2023 Len Popp - see LICENSE
