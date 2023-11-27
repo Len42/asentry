a2ed276a594acc50500d15cf252ec306a8604925
Len Popp len.popp@gmail.com <len.popp@gmail.com>
2023-11-26 20:37:51 -0500

    add a note about "press Enter"

--- asentry.py
+++ asentry.py
@@ -7,6 +7,11 @@
     If there is a sound file named "alert.mp3" in this directory, it will be
     played when a warning message is displayed.
 
+    If any warning messages are displayed, the program waits for the user to
+    press Enter before exiting; otherwise it exits immediately. This is because
+    I have the program run at system startup and I need its window to stay open
+    so I can read the messages.
+
     Copyright 2023 Len Popp - see LICENSE
 """
 
